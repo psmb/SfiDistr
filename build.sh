46dbf79c97f29b3ab94121d88a896c621eb4a12d
Dmitri Pisarev <dimaip@gmail.com>
2014-11-26 11:06:22 +0300

    Move import code to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-#
-# Site build script
 
-cd ~/typo3-app
+ssh dimaip@server.psmb.ru "cd /www/dev.sfi.ru/SfiDistr;./flow site:export --tidy --filename AutoExport/Site.xml;"
+cd ~/sfi-dev
+rsync -avz dimaip@server.psmb.ru:/www/dev.sfi.ru/SfiDistr/AutoExport/ AutoExport
 rm -rf Data/Temporary/Development/Cache/
-touch 1234
+./flow site:prune
+./flow site:import --filename AutoExport/Site.xml
+rm -rf Data/Temporary/Development/Cache/
+
