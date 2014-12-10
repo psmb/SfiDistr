ef4d65a6e86028a4ab5aec2811a21d64afe8c437
Dmitri Pisarev <dimaip@gmail.com>
2014-12-10 21:04:02 +0300

    Update import script

--- import.sh
+++ import.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+#Export dump stuff from server, sync and import.
 
-ssh dimaip@server.psmb.ru "cd /www/dev.sfi.ru/SfiDistr;./flow site:export --tidy --filename AutoExport/Site.xml;"
-cd ~/sfi-dev
-rsync -avz dimaip@server.psmb.ru:/www/dev.sfi.ru/SfiDistr/AutoExport/ AutoExport
+set -e
+set -u
+
+ssh dimaip@server.psmb.ru "cd /www/sfi.ru/SfiDistr;./flow site:export --tidy --filename AutoExport/Site.xml;"
+rsync -avz dimaip@server.psmb.ru:/www/sfi.ru/SfiDistr/AutoExport/ AutoExport
 rm -rf Data/Temporary/Development/Cache/
 ./flow site:prune
 ./flow site:import --filename AutoExport/Site.xml
-rm -rf Data/Temporary/Development/Cache/
-
+rm -rf Data/Temporary
