52a62d54edaee502b40b400d5f8da54ada02c91f
Dmitri Pisarev <dimaip@gmail.com>
2014-12-12 03:33:59 +0300

    Configure Surf

---
+++ Build/Surf/Sfi.php
@@ -0,0 +1,23 @@
+<?php
+use TYPO3\Surf\Domain\Model\Workflow;
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\SimpleWorkflow;
+
+/** @var \TYPO3\Surf\Domain\Model\Deployment $deployment */
+
+$workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
+$deployment->setWorkflow($workflow);
+
+
+$node = new \TYPO3\Surf\Domain\Model\Node('sfi');
+$node->setHostname('sfi.ru');
+$node->setOption('username', 'dimaip');
+
+$application = new \TYPO3\Surf\Application\TYPO3\Flow();
+$application->setDeploymentPath('/www/sfi.ru/surf');
+$application->setOption('repositoryUrl', 'git@github.com:sfi-ru/SfiDistr.git');
+$application->setOption('composerCommandPath', '/usr/local/bin/composer');
+$application->addNode($node);
+
+$deployment->addApplication($application);
+?>
