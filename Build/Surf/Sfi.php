701fac660c75f3a2080f1df68e217b653090cafe
Dmitri Pisarev <dimaip@gmail.com>
2015-01-26 21:58:45 +0300

    Updated Surf deployment

--- Build/Surf/Sfi.php
+++ Build/Surf/Sfi.php
@@ -6,6 +6,19 @@ use TYPO3\Surf\Domain\Model\SimpleWorkflow;
 /** @var \TYPO3\Surf\Domain\Model\Deployment $deployment */
 
 $workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
+
+$smokeTestOptions = array(
+        'url' => 'http://sfi.ru',
+        'remote' => TRUE,
+        'expectedStatus' => 200,
+        'expectedRegexp' => '/Page--Main/'
+);
+$workflow->defineTask('sfi.sfi:smoketest', 'typo3.surf:test:httptest', $smokeTestOptions);
+$workflow->defineTask('sfi.sfi:initialize',
+        'typo3.surf:shell',
+        array('command' => 'cd {releasePath} && cp Configuration/Production/Settings.yaml Configuration/Settings.yaml && FLOW_CONTEXT=Production ./flow flow:cache:flush --force && chmod g+rwx -R . && FLOW_CONTEXT=Production ./flow cache:warmup')
+);
+
 $deployment->setWorkflow($workflow);
 
 
@@ -14,10 +27,18 @@ $node->setHostname('server.psmb.ru');
 $node->setOption('username', 'dimaip');
 
 $application = new \TYPO3\Surf\Application\TYPO3\Flow();
+
+
 $application->setDeploymentPath('/www/sfi.ru/surf');
 $application->setOption('repositoryUrl', 'git@github.com:sfi-ru/SfiDistr.git');
 $application->setOption('composerCommandPath', '/usr/local/bin/composer');
 $application->addNode($node);
 
+$deployment->onInitialize(function() use ($workflow, $application) {
+	$workflow->addTask('sfi.sfi:initialize', 'migrate', $application);
+	$workflow->addTask('sfi.sfi:smoketest', 'test', $application);
+});
 $deployment->addApplication($application);
+
+
 ?>
