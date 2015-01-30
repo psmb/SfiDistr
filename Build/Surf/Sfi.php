a1f9693fb0bdbb1056f0577a71493a9908634402
Dmitri Pisarev <dimaip@gmail.com>
2015-01-30 18:59:34 +0300

    Circle

--- Build/Surf/Sfi.php
+++ Build/Surf/Sfi.php
@@ -3,10 +3,20 @@ use TYPO3\Surf\Domain\Model\Workflow;
 use TYPO3\Surf\Domain\Model\Node;
 use TYPO3\Surf\Domain\Model\SimpleWorkflow;
 
-/** @var \TYPO3\Surf\Domain\Model\Deployment $deployment */
+$application = new \TYPO3\Surf\Application\TYPO3\Flow('SfiDistr');
+$application->setDeploymentPath('/www/sfi.ru/surf');
+$application->setOption('repositoryUrl', 'git@github.com:sfi-ru/SfiDistr.git');
+$application->setOption('composerCommandPath', '/usr/local/bin/composer');
+$application->setOption('keepReleases', 10);
+
+// Use rsync for transfer instead of composer
+$application->setOption('transferMethod', 'rsync');
+$application->setOption('packageMethod', 'git');
+$application->setOption('updateMethod', NULL);
+$application->setOption('rsyncFlags', "--recursive --omit-dir-times --perms --links --delete --delete-excluded --exclude '.git'");
+
 
 $workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
-
 $workflow->defineTask('sfi.sfi:beard',
         'typo3.surf:localshell',
         array('command' => 'cd {workspacePath} && git config --global user.email "dimaip@gmail.com" &&  git config --global user.name "Dmitri Pisarev (CircleCI)" && ./beard patch')
@@ -22,30 +32,19 @@ $smokeTestOptions = array(
         'expectedRegexp' => '/Page--Main/'
 );
 $workflow->defineTask('sfi.sfi:smoketest', 'typo3.surf:test:httptest', $smokeTestOptions);
-
-$node = new \TYPO3\Surf\Domain\Model\Node('sfi');
-$node->setHostname('server.psmb.ru');
-$node->setOption('username', 'dimaip');
-
-$application = new \TYPO3\Surf\Application\TYPO3\Flow();
-$application->setDeploymentPath('/www/sfi.ru/surf');
-$application->setOption('repositoryUrl', 'git@github.com:sfi-ru/SfiDistr.git');
-$application->setOption('composerCommandPath', '/usr/local/bin/composer');
-$application->setOption('keepReleases', 10);
-
-// Use rsync for transfer instead of composer
-$application->setOption('transferMethod', 'rsync');
-$application->setOption('packageMethod', 'git');
-$application->setOption('updateMethod', NULL);
-$application->setOption('rsyncFlags', "--recursive --omit-dir-times --perms --links --delete --delete-excluded --exclude '.git'");
-
-$application->addNode($node);
-
 $workflow->beforeStage('transfer', 'sfi.sfi:beard', $application);
 $workflow->addTask('sfi.sfi:initialize', 'migrate', $application);
 $workflow->addTask('sfi.sfi:smoketest', 'test', $application);
 $workflow->setEnableRollback(FALSE);
 
+
+$node = new \TYPO3\Surf\Domain\Model\Node('sfi');
+$node->setHostname('server.psmb.ru');
+$node->setOption('username', 'dimaip');
+$application->addNode($node);
+
+
+/** @var \TYPO3\Surf\Domain\Model\Deployment $deployment */
 $deployment->setWorkflow($workflow);
 $deployment->addApplication($application);
 
