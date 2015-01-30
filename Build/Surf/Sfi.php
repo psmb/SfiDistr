557d3af74cbb5707864398211ca5e7ffdc15a2d2
Dmitri Pisarev <dimaip@gmail.com>
2015-01-30 17:32:47 +0300

    CircleCI

--- Build/Surf/Sfi.php
+++ Build/Surf/Sfi.php
@@ -9,7 +9,7 @@ $workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
 
 $workflow->defineTask('sfi.sfi:beard',
         'typo3.surf:shell',
-        array('command' => 'cd {releasePath} && ./beard patch')
+        array('command' => '/home/ubuntu/SfiDistr && ./beard patch')
 );
 $workflow->defineTask('sfi.sfi:initialize',
         'typo3.surf:shell',
