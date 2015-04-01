f39fa6507b2b296807ccdce4fe0a6ee23468458e
Dmitri Pisarev <dimaip@gmail.com>
2015-04-01 21:43:26 +0300

    Adjust loading of backend js

--- Packages/Sites/Sfi.Sfi/Frontend/js/backend.js
+++ Packages/Sites/Sfi.Sfi/Frontend/js/backend.js
@@ -5,9 +5,12 @@ var backendOnlyStuff = function() {
     });
 };
 
-
 if (typeof document.addEventListener === 'function') {
 	document.addEventListener('Neos.PageLoaded', function(event) {
 		backendOnlyStuff();
 	}, false);
 }
+
+$(document).ready(function () {
+	backendOnlyStuff();
+});
