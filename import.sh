66d574d40d07b3ff4e4145085b1b845e19e0a58d
Dmitri Pisarev <dimaip@gmail.com>
2015-04-20 11:31:20 +0300

    Adjust import.sh

--- import.sh
+++ import.sh
@@ -8,3 +8,4 @@ set -u
 #ssh www@server.psmb.ru -p 1122 'cd /data/www/sfi.ru/surf/releases/current/ && ./flow db:export --mode=all --sql-file="Data/Persistent/db.sql"'
 rsync -rlhvz -e "ssh -p 2222" www@server.psmb.ru:/data/www/sfi.ru/surf/shared/Data/Persistent/ Data/Persistent/
 ./flow db:import --sql-file="Data/Persistent/db.sql"
+./flow flow:cache:flush --force
