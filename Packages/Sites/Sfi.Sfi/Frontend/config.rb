d4dc77424ed02609c54e12844ef49db2564a8973
Yuri Zaveryukha <zaveryukha@gmail.com>
2015-09-23 08:03:31 +0300

    Print version 0.1

--- Packages/Sites/Sfi.Sfi/Frontend/config.rb
+++ Packages/Sites/Sfi.Sfi/Frontend/config.rb
@@ -8,6 +8,7 @@ css_dir = "Public/Built"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
+Encoding.default_external = "utf-8"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
