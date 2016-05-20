e6f6f64077f6cba28a7c22c9916aaa0b81529f45
Dmitri Pisarev <dimaip@gmail.com>
2016-05-20 18:11:14 +0300

    FEATURE: load more images in footer gallery

--- Packages/Sites/Sfi.Sfi/Frontend/js/plugins.js
+++ Packages/Sites/Sfi.Sfi/Frontend/js/plugins.js
@@ -270,3 +270,43 @@ if (typeof document.addEventListener === 'function') {
 		onReadyPlugins();
 	}, false);
 }
+
+
+// FooterGallery
+(function () {
+  'use strict';
+
+  Array.prototype.forEach.call(document.getElementsByClassName('js-FooterGallery'), function (item) {
+    FooterGallery(item);
+  });
+
+  function FooterGallery(node) {
+    var nextPage = 1;
+    var content = node.querySelector('.js-FooterGallery-content');
+    var loadMore = node.querySelector('.js-FooterGallery-loadMore');
+    if (loadMore && content) {
+      loadMore.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        var request = new XMLHttpRequest();
+        request.open('GET', '/?galleryPage=' + nextPage, true);
+        loadMore.innerHTML = loadMore.attributes['data-loading'].value;
+        loadMore.disabled = true;
+        request.onload = function () {
+          if (this.status >= 200 && this.status < 400) {
+            var resp = JSON.parse(this.response);
+            loadMore.innerHTML = loadMore.attributes['data-loadMore'].value;
+            content.innerHTML += resp.content;
+            loadMore.disabled = false;
+            nextPage++;
+            // If nothing left to load
+            if (!resp.loadMore) {
+              loadMore.innerHTML = loadMore.attributes['data-done'].value;
+              loadMore.disabled = true;
+            }
+          }
+        };
+        request.send();
+      });
+    }
+  }
+}());
