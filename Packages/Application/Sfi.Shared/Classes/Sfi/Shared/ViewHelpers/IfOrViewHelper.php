66135ca36a5adb369e259830fb7f83d5e3604dc8
Dmitri Pisarev <dimaip@gmail.com>
2017-02-02 10:02:51 +0000

    TASK: Apply migration TYPO3.FluidAdaptor-20161130112935 to package "Sfi.Shared"
    Adjusts code to package renaming from "TYPO3.Fluid" to
    "Neos.FluidAdaptor".

--- Packages/Application/Sfi.Shared/Classes/Sfi/Shared/ViewHelpers/IfOrViewHelper.php
+++ Packages/Application/Sfi.Shared/Classes/Sfi/Shared/ViewHelpers/IfOrViewHelper.php
@@ -2,7 +2,7 @@
 namespace Sfi\Shared\ViewHelpers;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "TYPO3.Fluid".           *
+ * This script belongs to the TYPO3 Flow package "Neos.FluidAdaptor".           *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -11,7 +11,7 @@ namespace Sfi\Shared\ViewHelpers;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use TYPO3\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+use Neos\FluidAdaptor\Core\ViewHelper\AbstractConditionViewHelper;
 
 class IfOrViewHelper extends AbstractConditionViewHelper {
 
