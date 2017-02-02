ae9fc885f85dc2d3f6cd047e1cb5e9db411c9326
Dmitri Pisarev <dimaip@gmail.com>
2017-02-02 19:45:47 +0300

    BUGFIX: ifOr VH

--- Packages/Application/Sfi.Shared/Classes/Sfi/Shared/ViewHelpers/IfOrViewHelper.php
+++ Packages/Application/Sfi.Shared/Classes/Sfi/Shared/ViewHelpers/IfOrViewHelper.php
@@ -6,14 +6,21 @@ use Neos\FluidAdaptor\Core\ViewHelper\AbstractConditionViewHelper;
 class IfOrViewHelper extends AbstractConditionViewHelper {
 
     /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('condition', 'mixed', 'Condition 1', false);
+        $this->registerArgument('or', 'mixed', 'Condition 2', false);
+    }
+
+    /**
      * renders <f:then> child if $condition or $or is true, otherwise renders <f:else> child.
      *
-     * @param boolean $condition View helper condition
-     * @param boolean $or View helper condition
      * @return string the rendered string
      */
-    public function render($condition, $or) {
-        if ($condition || $or) {
+    public function render() {
+        if ($this->arguments['condition'] || $this->arguments['or']) {
             return $this->renderThenChild();
         } else {
             return $this->renderElseChild();
