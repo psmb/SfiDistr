9997b559deb0a5aaf294b159f8fcdebed6e6f7e0
Dmitri Pisarev <dimaip@gmail.com>
2018-05-02 12:36:34 +0300

    A few adjustments

--- Packages/Sites/Sfi.Sfi/Classes/Sfi/Sfi/Service/EventStoreApi.php
+++ Packages/Sites/Sfi.Sfi/Classes/Sfi/Sfi/Service/EventStoreApi.php
@@ -3,7 +3,7 @@ namespace Sfi\Sfi\Service;
 
 class EventStoreApi {
     public function getPending($type) {
-        return json_decode($this->callAPI('GET', 'http://eventstore:2113/projection/pendingEmails-' . $type . '/result'), true) ?? [];
+        return json_decode($this->callAPI('GET', 'http://eventstore:2113/projection/pendingEmails-' . $type), true) ?? [];
     }
 
     public function registerEmailSent($reason, $type, $email)
